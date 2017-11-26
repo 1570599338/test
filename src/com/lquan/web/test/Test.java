5b2667e0b27a1f3640b41f306417b656a4fed05f
1570599338@qq.com <1570599338@qq.com>
2017-11-27 01:02:33 +0800

    test

---
+++ src/com/lquan/web/test/Test.java
@@ -0,0 +1,15 @@
+package com.lquan.web.test;
+
+/**
+ * test
+ * @author lquan
+ *
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
